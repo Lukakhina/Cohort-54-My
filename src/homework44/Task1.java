7f118b896a8c9288a061d3ebc45354ea88fcf400
Lukakhina Tetiana <tanya555work@gmail.com>
2025-03-11 16:43:18 +0200

    10.03.22 1-Task homework

--- src/homework44/Task1.java
+++ src/homework44/Task1.java
@@ -12,19 +12,56 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit.
 Lorem ipsum dolor sit.
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo!
 Copy
-Ваша задача используя классы пакета java.io создать файл с именем 'copy.txt'
- и скопировать в него все данные из файла 'text.txt'
+Ваша задача используя классы пакета java.io создать файл с именем
+'copy.txt' и скопировать в него все данные из файла 'text.txt'
  */
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 public class Task1 {
     public static void main(String[] args) {
-        File file = new File("text.txt");
-        System.out.println(file.exists());
+        File fileText = new File("text.txt");
+        try {
+            System.out.println("Создаем файл  'text.txt'" + fileText.createNewFile());
+            FileWriter fileWriter = new FileWriter(fileText);
+            fileWriter.write("Lorem ipsum dolor sit amet, consectetur adipisicing.\n" +
+                    "Lorem ipsum dolor sit amet.\n" +
+                    "Lorem ipsum dolor sit amet, consectetur adipisicing elit.\n" +
+                    "Lorem, ipsum dolor.\n" +
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia.\n" +
+                    "Lorem, ipsum.\n" +
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit.\n" +
+                    "Lorem ipsum dolor sit.\n" +
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo!");
+            fileWriter.close();
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileText));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Файл 'text.txt' создан");
+        File fileCopy = new File("copy.txt");
+        try {
+            System.out.println("Создаем файл  'copy.txt'" + fileCopy.createNewFile());
+            FileWriter fileWriter = new FileWriter(fileCopy);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileText));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                fileWriter.write(line + "\n");
+            }
+            fileWriter.close();
+            bufferedReader.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
-
-
 }
